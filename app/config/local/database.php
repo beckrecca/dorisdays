975324b8b3462179fa5a53f5f4acb0ae246ef6e1
beckrecca <rebecca.doris42@gmail.com>
2015-01-09 18:11:56 -0500

    weird database error

--- app/config/local/database.php
+++ app/config/local/database.php
@@ -23,6 +23,7 @@ return array(
 		'mysql' => array(
 			'driver'    => 'mysql',
 			'host'      => 'localhost',
+			'unix_socket'   => '/Applications/MAMP/tmp/mysql/mysql.sock',
 			'database'  => 'dorisdays',
 			'username'  => 'root',
 			'password'  => 'root',
