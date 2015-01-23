9406c163b6b500cd7a4fd33799163d61acfb1656
beckrecca <rebecca.doris42@gmail.com>
2015-01-23 08:17:27 -0500

    started working on passwordreset functionality

--- app/routes.php
+++ app/routes.php
@@ -62,6 +62,26 @@ Route::post('/edit_profile', 'UserController@handleEdit');
 
 Route::get('logout', 'UserController@logout');
 
+Route::get('password/reset', array(
+  'uses' => 'PasswordController@remind',
+  'as' => 'password.remind'
+));
+
+Route::post('password/reset', array(
+  'uses' => 'PasswordController@request',
+  'as' => 'password.request'
+));
+
+Route::get('password/reset/{token}', array(
+  'uses' => 'PasswordController@reset',
+  'as' => 'password.reset'
+));
+
+Route::post('password/reset/{token}', array(
+  'uses' => 'PasswordController@update',
+  'as' => 'password.update'
+));
+
 # TESTING
 
 Route::get('whoops', function() {
